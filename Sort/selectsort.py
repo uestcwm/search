00e60d76e79a06fdded0fb0ea8bcb7e9c58652eb
一款可以备份重要文件的小程序 <41863216+uestcwm@users.noreply.github.com>
2019-04-23 15:39:45 +0800

    Update selectsort.py

--- Sort/selectsort.py
+++ Sort/selectsort.py
@@ -1,20 +1,14 @@
 '''
 选择排序，速度不是很快
+O(n^2)
 '''
-def findsmallest(arr):  #返回最小元素的下标
-    smallest = arr[0]  #存储最小的值
-    smallest_index = 0   #存储最小元素的索引
-    for i in range(1,len(arr)):
-        if arr[i] < smallest:
-            smallest = arr[i]
-            smallest_index = i
-    return smallest_index  
+def selectsort(seq):
+    for fillslot in range(len(seq)-1,0,-1):
+        pos = 0
+        for loc in range(1,fillslot):
+            if seq[loc] > seq[pos]:
+                pos = loc
+        seq[pos], seq[fillslot] = seq[fillslot], seq[pos]
+    return seq
 
-def selectionsort(arr):
-    newarr = []   #空列表用来存储排序后的
-    for i in range(len(arr)):
-        smallest = findsmallest(arr)  #此时smallest是下标
-        newarr.append(arr.pop(smallest))   #pop(下标)将下标元素删除
-    return newarr  
-
-selectionsort([5,3,6,2,9])
+selectsort([5,3,6,2,9])
