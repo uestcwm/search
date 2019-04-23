bd08d1fd4aca939b1f4fa7320976921c73f0a595
一款可以备份重要文件的小程序 <41863216+uestcwm@users.noreply.github.com>
2019-04-23 17:31:03 +0800

    Create countsort.py

---
+++ Sort/countsort.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+#coding=utf-8
+'''
+ 桶排序
+ 复杂度O(n)
+ '''
+def countsort( A, d ):  #A待排数组， d位数 如果位置需要先修改一下知道A中最多位数
+    for k in range(d):  #k轮排序
+        s[ [] for i in range(10) ]  #数字0~9，建10个桶
+        '''对于数组中的元素，首先按照最低有效数字进行
+           排序，然后由低位向高位进行'''
+        for i in A:
+            s[ i//(10**k)%10 ].append(i)
+        A = [j for i in s for j in i]  #977/10=97(小数舍去),87/100=0
+    return A
+
+from random import randint
+a = [randint(1,999) for i in range(10)]  #产生10个随机3位数
+a = RadixSort(a,3)
+print(a)
+
+[181, 263, 893, 705, 566, 727, 801, 310, 589, 783]  #随机数
+[181, 263, 310, 566, 589, 705, 727, 783, 801, 893]  #排序后
